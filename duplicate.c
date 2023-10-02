cc035f19cf795aec5864a6c3e9da28b2ba6f1965
Berna Kiziler <bkiziler@k1m37s01.42istanbul.com.tr>
2023-10-02 10:03:52 +0300

    needs more fixing

--- duplicate.c
+++ duplicate.c
@@ -6,7 +6,7 @@
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 20:06:10 by bkiziler          #+#    #+#             */
-/*   Updated: 2023/10/01 20:57:47 by bkiziler         ###   ########.fr       */
+/*   Updated: 2023/10/02 10:02:59 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,29 @@ t_data	*g_data;
 
 void	duplication(char *input, char *output)
 {
-	int	fd_in = 0;
-	int	fd_out = 0;
+	static int	change = 0;
+	int			fd_in;
+	int			fd_out;
+	int			std_in;
+	int			std_out;
 
+	if (change == 0)
+	{
+		std_in = dup(0);
+		std_out = dup(1);
+		change = 1;
+	}
 	if (input != NULL)
 	{
-		fd_in = open(input, O_RDONLY, 0777);
-		fd_in = dup2(fd_in, 0);
-		if (fd_in < 0)
+		fd_in = open(input, O_RDONLY | O_APPEND, 0777);
+		dup2(fd_in, 0);
+		if (dup2(fd_in, 0) < 0)
+			exit_program("Fd Error.", -1);
+		close(fd_in);
+	}
+	else
+	{
+		if (dup2(std_in, 0) < 0)
 			exit_program("Fd Error.", -1);
 	}
 	if (output != NULL)
@@ -31,10 +46,15 @@ void	duplication(char *input, char *output)
 		if (g_data->append_flag)
 			fd_out = open(output, O_WRONLY | O_APPEND, 0777);
 		else
-			fd_out = open(output, O_WRONLY, 0777);
-		fd_out = dup2(fd_out, 1);
-		if (fd_out < 0)
+			fd_out = open(output, O_WRONLY | O_APPEND, 0777);//direkt silip yazması için flag gerekiyor
+		dup2(fd_out, 1);
+		if (dup2(fd_out, 1); < 0)
+			exit_program("Fd Error.", -1);
+		close(fd_out);
+	}
+	else
+	{
+		if (dup2(std_out, 1) < 0)
 			exit_program("Fd Error.", -1);
 	}
-	printf("in: %d, out: %d\n", fd_in, fd_out);
 }
